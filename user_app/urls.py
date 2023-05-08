dd3ab4081b7985cf6af0b4f43df0815d38c7d9ab
Jishna mk <jishnamk9@gmail.com>
2023-05-08 15:07:24 +0530

    added image to pdf converter

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from.import views
 
+from .views import ProductListPDF
+
+
 urlpatterns = [
     # path('',views.first,name='first'),
     path('',views.home,name='home'),
-    path('add_product',views.add_product,name='add_product')
+    path('add_product',views.add_product,name='add_product'),
+    path('product_list_pdf/', ProductListPDF.as_view(), name='product_list_pdf'),
+    
 ]
+
+
--- user_app/views.py
+++ user_app/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from.forms import ProductlistForm
 from django.contrib import messages
-from.models import Productlist
+
+from django.views.generic import View
+from django.views.generic import View
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
+from .models import Productlist
 
 # Create your views here.
 # def first(request):
@@ -27,3 +33,68 @@ def add_product(request):
             return redirect('add_product')
         
     return render(request,"add_product.html",{"add_form":form})
+
+
+
+# class ProductPDFView(View):
+#     def get(self, request, *args, **kwargs):
+#         # Create a HttpResponse object with content type PDF
+#         response = HttpResponse(content_type='application/pdf')
+#         # Set the Content-Disposition header to force download the PDF file
+#         response['Content-Disposition'] = 'attachment; filename="products.pdf"'
+#         # Create a canvas to draw on
+#         pdf_canvas = canvas.Canvas(response)
+        
+#         # Loop through all_products and draw each product on the canvas
+#         y = 750
+#         for product in Products:
+#             pdf_canvas.drawImage(product.Product_image.url, 50, y, width=100, height=100)
+#             pdf_canvas.drawString(200, y+50, product.Product_name)
+#             pdf_canvas.drawString(200, y+25, product.Product_category)
+#             pdf_canvas.drawString(400, y+50, f"${product.Product_price}")
+#             y -= 150
+        
+#         # Save the PDF canvas and return the HttpResponse object
+#         pdf_canvas.save()
+#         return response
+
+
+class ProductListPDF(View):
+    def get(self, request, *args, **kwargs):
+        # Get all products from the database
+        products = Productlist.objects.all()
+
+        # Create a HttpResponse object with content type PDF
+        response = HttpResponse(content_type='application/pdf')
+        # Set the Content-Disposition header to force download the PDF file
+        response['Content-Disposition'] = 'attachment; filename="product_list.pdf"'
+        # Create a canvas to draw on
+        pdf_canvas = canvas.Canvas(response, pagesize=letter)
+
+        # Set the font size and leading
+        pdf_canvas.setFont('Helvetica', 12)
+        pdf_canvas.setLineWidth(.3)
+        pdf_canvas.setStrokeColorRGB(0.5, 0.5, 0.5)
+
+        # Set the initial y-coordinate
+        y = 720
+
+        # Loop through all products and draw each product on the canvas
+        for product in products:
+            # Draw the product image
+            image = product.Product_image.path
+            pdf_canvas.drawImage(image, 50, y - 0.5 * inch, width=1.5 * inch, height=1.5 * inch)
+
+            # Draw the product name
+            pdf_canvas.drawString(100, y, product.Product_name)
+            # Draw the product category
+            pdf_canvas.drawString(100, y - 0.25 * inch, product.Product_category)
+            # Draw the product price
+            pdf_canvas.drawString(100, y - 0.5 * inch, product.Product_price)
+
+            # Decrease the y-coordinate for the next product
+            y -= 1.5 * inch
+
+        # Save the PDF canvas and return the HttpResponse object
+        pdf_canvas.save()
+        return response
